748de66651a4cc70f79bc2b83d764a982a5ec7a9
Semionn <mrsemionn@gmail.com>
2016-11-27 00:05:29 +0300

    fix some looks

--- src/looksService.js
+++ src/looksService.js
@@ -14,31 +14,43 @@ export class LooksService {
 
   init() {
     var type = "item";
-    var look1Item0 = this.parseArticlePromise(this.zalandoService.articleById("f1451b000-c11"), type);
-    var look1Item1 = this.parseArticlePromise(this.zalandoService.articleById("on321g0dm-n11"), type);
-    var look1Item2 = this.parseArticlePromise(this.zalandoService.articleById("gp021i03f-b11"), type);
-    var look1Item3 = this.parseArticlePromise(this.zalandoService.articleById("k0051e00p-o11"), type);
-    var look1Item4 = this.parseArticlePromise(this.zalandoService.articleById("gp021i03f-b11"), type);
     var look1 = {
       "id":0,
       "html": "#/look/0",
       "name": "Best look ever",
       "imageUrl":"https://cs7050.vk.me/c604428/v604428952/341d3/Qh_ul8SiIfY.jpg",
-      "items":[look1Item0, look1Item1, look1Item2, look1Item3, look1Item4]
+      "items":[]
     };
-    var look2Item0 = this.parseArticlePromise(this.zalandoService.articleById("ev451ba0t-q11"), type);
-    var look2Item1 = this.parseArticlePromise(this.zalandoService.articleById("ev421ia34-c11"), type);
-    var look2Item2 = this.parseArticlePromise(this.zalandoService.articleById("ev451ha49-q11"), type);
+    this.parseArticlePromise(this.zalandoService.articleById("f1451b000-c11"), type, look1);
+    this.parseArticlePromise(this.zalandoService.articleById("on321g0dm-n11"), type, look1);
+    this.parseArticlePromise(this.zalandoService.articleById("gp021i03f-b11"), type, look1);
+    this.parseArticlePromise(this.zalandoService.articleById("k0051e00p-o11"), type, look1);
+
     var look2 = {
       "id":1,
       "html": "#/look/1",
       "name": "Best look ever 2",
       "imageUrl":"https://pp.vk.me/c836637/v836637493/145d3/3gcHH2krBzA.jpg",
-      "items": [look2Item0, look2Item1, look2Item2]
+      "items": []
     };
-    
+    this.parseArticlePromise(this.zalandoService.articleById("ev451ba0t-q11"), type, look2);
+    this.parseArticlePromise(this.zalandoService.articleById("ev421ia34-c11"), type, look2);
+    this.parseArticlePromise(this.zalandoService.articleById("ev451ha49-q11"), type, look2);
+
+    var look3 = {
+      "id":2,
+      "html":"#/look/2",
+      "name": "Just the best",
+      "imageUrl":"https://pp.vk.me/c836637/v836637902/107c0/XyXRNES6Zc0.jpg",
+      "items":[]
+    }
+    this.parseArticlePromise(this.zalandoService.articleById("p2451e009-o12"), type, look3);
+    this.parseArticlePromise(this.zalandoService.articleById("ri921g00v-k11"), type, look3);
+
     this.addLook(look1);
     this.addLook(look2);
+    this.addLook(look3);
+
     this.initialized = true;
   }
 
@@ -62,13 +74,11 @@ export class LooksService {
     this.looks[look.id] = look;
   }
 
-  parseArticlePromise(promise, type) {
-    var temp = [];
+  parseArticlePromise(promise, type, look) {
     promise.then(article => {
-              temp.push(parseArticle(article, type));
+              look.items.push(parseArticle(article, type));
           },
           error => alert("Rejected: " + error)
     );
-    alert(temp.length);
   }
 }
